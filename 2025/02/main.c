a6efd06105bab30b0f4dff511241b7897715c0eb
Riibaii <244965364+Riibaii@users.noreply.github.com>
2025-12-10 04:28:10 +0800

    25-02 P2

--- 2025/02/main.c
+++ 2025/02/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+
+long pow10(int e) {
+    long ret = 1;
+    while (e--)
+        ret *= 10;
+    return ret;
+}
 
 int digit(long l) {
     int d = 1;
@@ -9,32 +15,63 @@ int digit(long l) {
     return d;
 }
 
-long invalid(long left, long right) {
-    /*  smallest first;
-        bigger and bigger until r */
+long helper(long start, long p, int n) {
     long sum = 0;
-    int d = digit(left);
-    long start_half;
-
-    if ((d % 2) == 0) {
-        start_half = left / pow(10, d/2); 
-        if (start_half + start_half * pow(10, digit(start_half)) < left)
-            start_half++;
-    } else {
-        start_half = pow(10, d/2);
-    }
-
-    long value;
-    while (1) {
-        value = start_half + start_half * pow(10, digit(start_half));
-        if (value > right)
-            break;
-        sum += value;
-        start_half++;
-        printf("\t%ld\n", value);
+    for (int i = 0; i < n; i++) {
+        sum *= p;
+        sum += start;
     }
     return sum;
 }
+
+/* generate instead of check */
+long getStart(long left, int n) {
+    int d = digit(left);
+    long start;
+
+    if ((d % n) == 0) {
+        long p = pow10(digit(left) / n);
+        start = left;
+        for (int i = 0; i < n - 1; i++)
+            start /= p;
+        if (helper(start, p, n) < left)
+            start++;
+    } else {
+        start = pow10(d / n);
+    }
+    return start;
+}
+
+/* faster for big numbers */
+long invalidN(long left, long right, int n) {
+    long sum = 0;
+    int d = digit(left);
+    long start = getStart(left, n);
+    long p = pow10(digit(start));
+    long value;
+    while (1) {
+        value = helper(start, p, n);
+        if (value > right || digit(start) * n > digit(right)) 
+            break;
+        sum += value;
+        start++;
+    }
+    return sum;
+}
+
+int isValid(long num) {
+    long n, p;
+    int d = digit(num);
+    for (int i = 2; i <= digit(num); i++) {
+        n = num;
+        p = pow10(d / i);
+        n %= p;
+        if (helper(n, p, i) == num) {
+            return 0;
+        }
+    }
+    return 1;
+} 
 
 int main (int argc, char * argv[]) {
     FILE * fp;
@@ -47,24 +84,26 @@ int main (int argc, char * argv[]) {
         printf("cannot open file!\n");
         return -1;
     }
-/*  invalid ID:
-        1. in range
-        2. digit repeat twice
-    input:
-        eg: 11-22,95-115,998-1012 */
 
     char range[100];
-    long l, r;
-    long sum = 0;
+    long l, r, num;
+    long sum_1 = 0;
+    long sum_2 = 0;
 
+    long tmp;
     while (1) {
         fscanf(fp, "%ld-%ld", &l, &r);
-        sum += invalid(l, r);
+        sum_1 += invalidN(l, r, 2);
+        for (num = l; num <= r; num++) {
+            if (!isValid(num)) {
+                sum_2 += num;
+            }
+        }
         if (fgetc(fp) != ',')
             break;
     }
 
     fclose(fp);
-    printf("%ld\n", sum);
-    return sum;
+    printf("P1:\t%ld\n", sum_1);
+    printf("P2:\t%ld\n", sum_2);
 }
