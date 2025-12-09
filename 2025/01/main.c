50bf471f2b0662fd8919518414615f770af8a253
Riibaii <244965364+Riibaii@users.noreply.github.com>
2025-12-09 23:03:15 +0800

    2025-01 P2

--- 2025/01/main.c
+++ 2025/01/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SHIFT 50000
+/* for neg div */
+
 int helper(char * op) {
     int sign;
     (op[0] == 'L') ? (sign = -1) : (sign = 1);
@@ -19,15 +22,24 @@ int main(int argc, char * argv[]) {
         return -1;
     }
 
-    int pos = 50, count = 0, rot;
+    int pos = 50, p1 = 0, p2 = 0, rot, buf;
     char line[100];
     while(fgets(line, 100, fp)){
         rot = helper(line);
+        buf = pos;
         pos += rot;
-        if (pos % 100 == 0)
-            count++;
+        if (pos % 100 == 0) {
+            p1++;
+        }
+        if (buf % 100 == 0) {
+            p2 += abs(rot) / 100;
+        } else if (pos % 100 == 0) {
+            p2 += 1 + abs(rot) / 100;
+        } else 
+            p2 += abs((pos + SHIFT) / 100 - (buf + SHIFT) / 100);
     }
 
-    printf("%d", count);
+    printf("P1: \t%d\n", p1);
+    printf("P2: \t%d\n", p2);
     fclose(fp);
 }
