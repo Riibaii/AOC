ec453d2a5c07e3c823dcbd9dc4fa6b55fae233ea
Riibaii <244965364+Riibaii@users.noreply.github.com>
2025-12-15 02:43:49 +0800

    25-06 P2

--- 2025/06/main.c
+++ 2025/06/main.c
@@ -1,58 +1,64 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <math.h>
+#include <ctype.h>
 
 #define MAXLINE 10000
 #define MAXROW  5
-#define MAXCOL  2000
 
-long a[MAXROW][MAXCOL];
+long getnumber(char [MAXROW][MAXLINE], int);
 
 int main(int argc, char * argv[]) {
     FILE * fp;
     fp = fopen(argv[1],  "r");
 
-    if (fp != NULL) {
+    if (fp != NULL)
         printf("%s\topened\n", argv[1]);
-    } 
     else {
         printf("cannot open file!\n");
         return -1;
     }
 
-    char line[MAXROW][MAXLINE];
     long p1 = 0, p2 = 0;
-
     int row = 0, col = 0;
-    long val;
 
-    char *token, *ptr;
-    while(fgets(line[row], MAXLINE, fp)) {
-        col = 0;
-        token = strtok_r(line[row], " ", &ptr);
-        while (token != NULL) {
-            a[row][col++] = (row==MAXROW-1) ? \
-                            token[0] : atoi(token);
-            token = strtok_r(NULL, " ", &ptr);
+    char line[MAXROW][MAXLINE];
+    while(fgets(line[row++], MAXLINE, fp))
+        ;
+    long val, buf;
+    for (col=0; col<strlen(line[MAXROW-1]); col++) {
+        if (line[MAXROW-1][col] == '+') {
+            for (int i=0; i<MAXROW-1; i++)
+                p1 += atol(line[i]+col);
+            while ((val = getnumber(line, col))) {
+                p2 += val;
+                col++;
+            }
         }
-        a[row][col] = -1;
-        row++;
+        else if (line[MAXROW-1][col] == '*') {
+            buf = 1;
+            for (int i=0; i<MAXROW-1; i++)
+                buf *= atol(line[i]+col);
+            p1 += buf;
+            buf = 1;
+            while ((val = getnumber(line, col))) {
+                buf *= val;
+                col++;
+            }
+            p2 += buf;
+        }
     }
-
-    for (col=0; col<MAXCOL; col++) {
-        if (a[0][col] < 0) break;
-        if (a[4][col] == '+') {
-            p1 += a[0][col] + a[1][col] + a[2][col] + a[3][col];
-        }
-        else {
-            p1 += a[0][col] * a[1][col] * a[2][col] * a[3][col];
-        }
-    } 
-    printf("0%s\n4%s\n", line[0], line[4]);
-
     printf("P1:\t%ld\n", p1);
     printf("P2:\t%ld\n", p2);
 
     fclose(fp);
+}
+
+long getnumber(char line[MAXROW][MAXLINE], int col) {
+    char buf[MAXROW];
+    int i;
+    for (i=0; i<MAXROW; i++)
+        buf[i] = isdigit(line[i][col]) ? line[i][col] : ' ';
+    buf[i] = '\0';
+    return atol(buf);
 }
